18d10bf8d5db3b53abe9b7f2520ecf121bccee8b
Olad1109 <logos_b.9.4410y@mail.ru>
2024-03-03 22:25:57 +0400

    creating an alpha menu

--- src/main/java/com/example/CrptApi.java
+++ src/main/java/com/example/CrptApi.java
@@ -1,14 +1,68 @@
 // Объявляем пакет классов
 package main.java.com.example;
-// Объявляем класс нашего приложения, т. е. чертёж объекта, его описание.
+
+import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.Scanner;
+
+
 public class CrptApi {
-    // Объявляем public - модификатор доступа к данному классу и к классам
-    // данного пакета. Так же здесь используется ключевое слово static в
-    // качестве модификатора доступа к переменным и методам
-    public static void main (String[] args) {
-        // В главном методе main() применяем класс System, что позволяет создать
-        // потоки стандартного ввода, стандартного вывода и вывода ошибок.
-        System.out.println("Hello, World! Привет, мир!");
+
+    static Scanner userInput = new Scanner(System.in);
+    static String email, phone, password, inputDateLastScore;
+    static boolean externalBool;
+    static long daysNewScore;
+    public static File userActions, dataUserTextFile, pushNumberFile, moveUserOfFunds,
+    folderDebit, folderCredit, folderUser;
+    static int accrualSensor, lastSearchIndex, percentDayCountDeb, percentDayCountCr;
+
+    //private static final String BASE_URL = "https://ismp.crpt.ru/api/v3/lk/documents/create";
+    
+    public static void main(String[] argCommStr) throws IOException, ParseException {
+        CrptApi crptApi = new CrptApi();
+        crptApi.startMenu();
+    }
+
+    public void startMenu() throws IOException, ParseException {
+
+        System.out.println("Добро пожаловать в приложение «WorkHonestSign»!");
+        System.out.println("""
+                -----------------------------------------------------------------
+                1. Получить список товаров (recList).
+                2. Получить информацию по товару (recInfo).
+                3. Обновить информацию по товару (updateInfo).
+                4. Выход из приложения (exit).""");
+        while (!externalBool) {
+            System.out.println("""
+                    -----------------------------------------------------------------
+                    Ввод необходимой команды:""");
+            String command = userInput.next();
+            switch (command) {
+                case "recList" -> receiveList();
+                case "recInfo" -> receiveInfo();
+                case "updateInfo" -> updateInfo();
+                case "exit" -> exitTheApplication();
+            }
+        }
+        
+    }
+
+    private void receiveList() throws IOException, ParseException {
+        
+    }
+
+    private void receiveInfo() throws IOException, ParseException {
+
+    }
+
+    private void updateInfo() throws IOException, ParseException {
+
+    }
+
+    private void exitTheApplication() throws IOException, ParseException {
+        // Выход из среды выполнения кода
+        Runtime.getRuntime().exit(0);
     }
 
 }
