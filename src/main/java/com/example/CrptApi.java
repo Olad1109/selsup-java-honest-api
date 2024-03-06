ef5e870e0d80e152dd133fc1214df200a45decfb
Olad1109 <logos_b.9.4410y@mail.ru>
2024-03-06 23:56:33 +0400

    the beta version of the application

--- src/main/java/com/example/CrptApi.java
+++ src/main/java/com/example/CrptApi.java
@@ -1,68 +1,108 @@
-// Объявляем пакет классов
-package main.java.com.example;
+package com.example;
 
-import java.io.File;
+
+
 import java.io.IOException;
-import java.text.ParseException;
-import java.util.Scanner;
+import java.io.StringWriter;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.Semaphore;
 
+import org.codehaus.jackson.map.ObjectMapper;
 
+/**
+ * Выполняет запрос Post на адрес
+ */
 public class CrptApi {
+    //Адрес для передачи запроса
+    private final URI uri = URI.create("https://ismp.crpt.ru/api/v3/lk/documents/create");
+    //семофор для ограничения доступа
+    private final Semaphore semaphore;
+    //interval время на requestsLimit запросов
+    private final long interval;
+    //Для освобождения семафора с задержкой
+    Timer timer = new Timer(true);
 
-    static Scanner userInput = new Scanner(System.in);
-    static String email, phone, password, inputDateLastScore;
-    static boolean externalBool;
-    static long daysNewScore;
-    public static File userActions, dataUserTextFile, pushNumberFile, moveUserOfFunds,
-    folderDebit, folderCredit, folderUser;
-    static int accrualSensor, lastSearchIndex, percentDayCountDeb, percentDayCountCr;
-
-    //private static final String BASE_URL = "https://ismp.crpt.ru/api/v3/lk/documents/create";
-    
-    public static void main(String[] argCommStr) throws IOException, ParseException {
-        CrptApi crptApi = new CrptApi();
-        crptApi.startMenu();
-    }
-
-    public void startMenu() throws IOException, ParseException {
-
-        System.out.println("Добро пожаловать в приложение «WorkHonestSign»!");
-        System.out.println("""
-                -----------------------------------------------------------------
-                1. Получить список товаров (recList).
-                2. Получить информацию по товару (recInfo).
-                3. Обновить информацию по товару (updateInfo).
-                4. Выход из приложения (exit).""");
-        while (!externalBool) {
-            System.out.println("""
-                    -----------------------------------------------------------------
-                    Ввод необходимой команды:""");
-            String command = userInput.next();
-            switch (command) {
-                case "recList" -> receiveList();
-                case "recInfo" -> receiveInfo();
-                case "updateInfo" -> updateInfo();
-                case "exit" -> exitTheApplication();
-            }
+    public static void main(String[] args) {
+        CrptApi crptApi = new CrptApi(1, 1000);
+        try {
+            crptApi.sendPostDocumentCreate(null);
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
         }
-        
+    
     }
-
-    private void receiveList() throws IOException, ParseException {
-        
-    }
-
-    private void receiveInfo() throws IOException, ParseException {
-
-    }
-
-    private void updateInfo() throws IOException, ParseException {
+    /**
+     * @param requestsLimit количество запорсов
+     * @param interval      время на requestsLimit запросов
+     */
+    public CrptApi(int requestsLimit, long interval) {
+        this.interval = interval;
+        this.semaphore = new Semaphore(requestsLimit);
 
     }
 
-    private void exitTheApplication() throws IOException, ParseException {
-        // Выход из среды выполнения кода
-        Runtime.getRuntime().exit(0);
+    /**
+     * @param document Документ который передаем на удрес
+     */
+    public HttpResponse<String> sendPostDocumentCreate(Document document) throws InterruptedException, IOException {
+        //занимает семафор
+        semaphore.acquire();
+        //переводит обьект в json с помощью Jackson
+        ObjectMapper mapper = new ObjectMapper();
+        StringWriter stringWriter = new StringWriter();
+        mapper.writeValue(stringWriter, document);
+        //отправляет запрос
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(uri)
+                .POST(HttpRequest.BodyPublishers.ofString(stringWriter.toString()))
+                .build();
+        //устанавливает обработчик
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                semaphore.release();
+            }
+        };
+        //запускает
+        timer.schedule(task, interval);
+        //получает и возвращает ответ
+        return httpClient.send(request, HttpResponse.BodyHandlers.ofString());
     }
 
+    public record Document(Description description,
+                           String doc_id,
+                           String doc_status,
+                           ArrayList<Object> doc_type,
+                           boolean importRequest,
+                           String owner_inn,
+                           String participant_inn,
+                           String producer_inn,
+                           String production_date,
+                           String production_type,
+                           ArrayList<Product> products,
+                           String reg_date,
+                           String reg_number) {
+    }
+
+    public record Description(String participantInn) {
+
+    }
+
+    public record Product(String certificate_document,
+                          String certificate_document_date,
+                          String certificate_document_number,
+                          String owner_inn,
+                          String producer_inn,
+                          String production_date,
+                          String tnved_code,
+                          String uit_code,
+                          String uitu_code) {
+    }
 }
